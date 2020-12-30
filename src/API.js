a098c43229a104fee34199444ee7ef02b28b3e5c
sai-yamavaram <saikarthiksep01@gmail.com>
2020-12-30 18:20:32 +0530

    modified url

--- src/API.js
+++ src/API.js
@@ -1,4 +1,8 @@
-const API_URL = window.location.hostname==="localhost"? "http://localhost:1337":"https://travel-log-friend-server.vercel.app";
+const API_URL =
+	window.location.hostname === "localhost"
+		? "http://localhost:1337"
+		: "https://sai-travel-log-backend.herokuapp.com";
+// const API_URL = "https://sai-travel-log-backend.herokuapp.com";
 
 export async function listLogEntries() {
 	const response = await fetch(`${API_URL}/api/logs`);
@@ -7,18 +11,17 @@ export async function listLogEntries() {
 
 export async function createLogEntry(entry) {
 	const apiKey = entry.apiKey;
-	delete entry.apiKey
+	delete entry.apiKey;
 	const response = await fetch(`${API_URL}/api/logs`, {
 		method: "POST",
 		headers: {
 			"content-type": "application/json",
-			'X-API-KEY' : apiKey,
+			"X-API-KEY": apiKey,
 		},
 		body: JSON.stringify(entry),
 	});
 	const json = await response.json();
-	if(response.ok)
-	{
+	if (response.ok) {
 		return json;
 	}
 	const error = new Error(json.message);
